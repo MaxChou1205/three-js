691cd48e8bc061290df5be75f845f60641ac042d
MaxChou1205 <v2asault@yahoo.com.tw>
2023-10-05 00:11:35 +0800

    import three modules

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
@@ -15,6 +16,8 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
